fdc41b2a7aa3596bc8b21af7e7d89a67a8c374cd
fsmutimeer <ishpata@hotmail.com>
2023-07-11 22:43:49 +0500

    first commit

---
+++ src/test/java/er/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package er;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
